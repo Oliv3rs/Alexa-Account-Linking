a8c7104dbad11333c14adc8f7b4ca81e7a2dd15a
Ollie G <53061592+Oliv3rs@users.noreply.github.com>
2020-07-06 13:52:39 -0500

    Create index.js

---
+++ lambda/index.js
@@ -0,0 +1,141 @@
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'||
+      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+        (
+          handlerInput.requestEnvelope.request.intent.name === 'ConsejoIntent' ||
+          handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent' 
+          
+      );
+    },
+    async handle(handlerInput) {
+        let TokenAcceso = handlerInput.requestEnvelope.context.System.user.accessToken;
+         let outputSpeech = 'Mensaje default';
+        if(TokenAcceso === undefined){
+            var speechText ='Por favor utilice la aplicación de Alexa para autenticarse con su cuenta de Amazon para comenzar a usar esta skill.';
+            return handlerInput.responseBuilder
+            .speak(speechText)
+            .withLinkAccountCard()
+            .getResponse();
+            
+        } else {
+            let url = `https://api.amazon.com/user/profile?access_token=${TokenAcceso}`;
+            
+            /*
+            data.user_id : "amzn1.account.xxxxxx"
+            data.email : "ogonzalezj@grupoacir.com.mx"
+            data.name : "Oliver Gónzalez"
+            data.postal_code: "10400"
+            */
+
+            await getRemoteData(url)
+            .then((response) =>{
+                const data = JSON.parse(response);
+                 outputSpeech = `>Hola, ${data.name}, tu correo es: ${data.email} y tu codigo postal es ${data.postal_code}.`;
+            })
+            .catch((err) =>{
+                outputSpeech= err.message;
+            })
+        }
+        return handlerInput.responseBuilder
+            .speak(outputSpeech)
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Gracias por pedir ayuda, por ahora esto es un demo y no puedo ayudarte';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Adios!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const getRemoteData = function (url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith('https') ? require('https') : require('http');
+    const request = client.get(url, (response) => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error('Failed with status code: ' + response.statusCode));
+      }
+      const body = [];
+      response.on('data', (chunk) => body.push(chunk));
+      response.on('end', () => resolve(body.join('')));
+    });
+    request.on('error', (err) => reject(err))
+  })
+};
+
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `Lanzaste ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler,
+    )
+    .addErrorHandlers(
+        ErrorHandler,
+    )
+    .lambda();
